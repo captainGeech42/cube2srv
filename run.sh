a15a9c78326b3d6d975ae3de65e49f3ff8be4498
Zander Work <zander@zanderwork.com>
2021-06-15 22:33:53 -0700

    fixed sed issue, its dumb but works

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-sed -s "s/REPLACE_SERVERDESC_REPLACE/$SERVER_DESCRIPTION/g" server-init.cfg > server-init.cfg
-sed -s "s/REPLACE_SERVERPASS_REPLACE/$SERVER_PASSWORD/g" server-init.cfg > server-init.cfg
-sed -s "s/REPLACE_SERVERADMINPASS_REPLACE/$SERVER_ADMIN_PASSWORD/g" server-init.cfg > server-init.cfg
+sed -s "s/REPLACE_SERVERDESC_REPLACE/$SERVER_DESCRIPTION/g" server-init.cfg > a
+sed -s "s/REPLACE_SERVERPASS_REPLACE/$SERVER_PASSWORD/g" a > b
+sed -s "s/REPLACE_SERVERADMINPASS_REPLACE/$SERVER_ADMIN_PASSWORD/g" b > server-init.cfg
+
+rm -f a b
 
 ./bin_unix/linux_64_server
